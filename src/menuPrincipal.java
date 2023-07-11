d8014a16da3b7da9740759c895fbea2264c20838
Miguel-Paredes <os.miguelparedes@gmail.com>
2023-07-11 07:59:03 -0500

    update

--- src/menuPrincipal.java
+++ src/menuPrincipal.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class menuPrincipal {
     private javax.swing.JPanel JPanel;
@@ -6,11 +9,11 @@ public class menuPrincipal {
     private JLabel marca;
     private JLabel anio;
     private JLabel modelo;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
     private JButton retroceder;
     private JButton avanzar;
+    private JLabel readmarca;
+    private JLabel readanio;
+    private JLabel readmodelo;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("vehiculos");
@@ -18,5 +21,39 @@ public class menuPrincipal {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+        vehiculos Camry = new vehiculos("Camry",1992,"Toyota");
+        vehiculos Corolla = new vehiculos("Corolla",1966,"Toyota");
+        vehiculos Prius = new vehiculos("Prius",1997,"Toyota");
+        vehiculos Hilux = new vehiculos("Hilux",1968,"Toyota");
+        vehiculos Focus = new vehiculos("Focus",1998, "Ford");
+        vehiculos Mustang = new vehiculos("Mustang",1964,"Ford");
+        vehiculos Camaro = new vehiculos("Camaro",1966,"Chevrolet");
+        List<vehiculos> lista = new ArrayList<vehiculos>();
+        lista.add(Camry);
+        lista.add(Corolla);
+        lista.add(Prius);
+        lista.add(Hilux);
+        lista.add(Focus);
+        lista.add(Mustang);
+        lista.add(Camaro);
+        /*String filePath="datos.dat";
+        try (FileOutputStream fileOut=new FileOutputStream(filePath);
+             ObjectOutputStream obOut=new ObjectOutputStream(fileOut);
+        ) {
+            obOut.writeObject(lista);
+            System.out.println("archivo escrito correctamente");
+        }catch(IOException e){
+            throw new RuntimeException(e);
+        }
+        try(FileInputStream fileIn=new FileInputStream(filePath);
+            ObjectInputStream objIn= new ObjectInputStream(fileIn);
+        ){
+            vehiculos readObject=(vehiculos) objIn.readObject();
+            System.out.println("El objeto en disco es: \n"+readObject);
+        }catch(IOException e){
+            throw new RuntimeException(e);
+        }catch (ClassNotFoundException e){
+            throw new RuntimeException();
+        }*/
     }
 }
